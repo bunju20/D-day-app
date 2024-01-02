7a2332ba73c66bf4a7170bdd778ef6296fba9489
bunju20 <ghkd2271@naver.com>
2024-01-02 15:19:32 +0900

    fix: 디자인 수정

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -8,47 +8,85 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         backgroundColor: Color(0xFFF7BCCF),
         body: SafeArea(
+          bottom: false,
           child: Container(
             width: MediaQuery.of(context).size.width,
             child: Column(
               children: [
-                Text(
-                  'U&I',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Parisienne',
-                    fontSize: 80.0,
-                  ),
-                ),
-                Text(
-                  "우리 처음 만난 날",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Sunflower',
-                    fontSize: 30.0,
-                  ),
-                ),
-                Text(
-                  "2021.12.27",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Sunflower',
-                    fontSize: 20.0,
-                  ),
-                ),
-                IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
-                Text(
-                  "D+1",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Sunflower',
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
+                _TopPart(),
+                _BottomPart(),
               ],
             ),
           ),
         ));
   }
 }
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'U&I',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Parisienne',
+              fontSize: 80.0,
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                "우리 처음 만난 날",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Sunflower',
+                  fontSize: 30.0,
+                ),
+              ),
+              Text(
+                "2021.12.27",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Sunflower',
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+          IconButton(
+              iconSize: 50.0,
+              onPressed: () {},
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.pink,
+              )),
+          Text(
+            "D+1",
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Sunflower',
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Image.asset('asset/img/middle_image.png'));
+  }
+}
+
